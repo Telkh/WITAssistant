1ef83f370b018837a83f5162c3016ef193e3e06c
jesgueva <jesgueva@gmail.com>
2018-11-28 22:44:56 -0500

    GPA_added static methods

--- src/gpaCalculator/GPA.java
+++ src/gpaCalculator/GPA.java
@@ -1,41 +1,70 @@
 package gpaCalculator;
 
+import java.util.ArrayList;
+
 public class GPA {
 	private final String [] GRADE = {"A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "F"}; //grades
 	private double [] weight = new double[GRADE.length]; //weight of letter grades
+	private String course;
+	private String grade;
+	private int credits;
 	
+	public GPA(String course, String grade, int credits) {
+		 generateValue();
+		 this.course = course;
+		 this.grade = grade;
+		 this.credits = credits;
+		 //this.getPoints(this.credits, this.grade);
+		 
+	 }
 	
-	
-	
-//	 public GPA(String course, String earnedGrade, int credits) {
-//		 generateValue();
-//		 this.course = course;
-//		 this.earnedGrade = earnedGrade;
-//		 this.credits = credits;
-//		 
-//	 }
-	 
 	public GPA() {
 		generateValue();
 	}
+	
+	 public String getGrade() {
+			return grade;
+		}
+
+
+		public void setGrade(String grade) {
+			this.grade = grade;
+		}
+
+
+		public int getCredits() {
+			return credits;
+		}
+
+		public void setCredits(int credits) {
+			this.credits = credits;
+		}
+
+		
+		 public String getCourse(){
+			 return course;
+		 }
 		 
 
+		public void setCourse(String course) {
+			this.course = course;
+		}
 
 	/*
 	 * Generates the weights of each letter grade
 	 */
 	void generateValue() {
-		weight[0] = 4.00;
-		double diff = 0.3333;
+		weight[0] = 4.00; //Maximum points
+		final double diff = 0.3333; //difference between grades
 		for (int i = 1; i < weight.length; i++) {
-			weight[i] = weight[i-1] - diff; 
+			weight[i] = weight[i-1] - diff;
 		}
 	}
 	/*
 	 * Finds the total points earned in the course
 	 */
-	public double getEarned(int numCredits, String grade) {
-		double points = numCredits * getWeight(grade);
+	public double getPoints() {
+		double points = this.credits * getWeight(this.grade);
 		return points;
 	}
 	
@@ -44,16 +73,25 @@ public class GPA {
 	 */
 	public double getWeight(String gradeInput) {
 		int i = 0;
-		while(gradeInput.equalsIgnoreCase(GRADE[i])) {
+		while(!gradeInput.equalsIgnoreCase(GRADE[i])) {
 			i++;
 		}
 		return weight[i];
 	}
-	
-//	public String toString() {
-//		System.out.printf("The grade for %s is %s with = weight %f, points %f", getWeight("A+"), getEarned(4,  "A+"));
-//		
-//		return "";
-//	}
+	public static int sumCredits(ArrayList<GPA> courses) {
+		int sum = 0;
+		for (int i = 0; i < courses.size(); i++) {
+			sum += courses.get(i).credits;
+		}
+		return sum;
+	}
+	public static double getAverage(ArrayList<GPA> courses) {
+		double sum = 0;
+		for (int i = 0; i < courses.size(); i++) {
+			sum += courses.get(i).getPoints();
+		}
+		double average = sum/sumCredits(courses); //average points / sum of credits.
+		return average;
+	}
 	
 }
