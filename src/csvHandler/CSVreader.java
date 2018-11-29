ee1cdbebaf17174c1d62a796e8fa925d10558eb0
jesgueva <jesgueva@gmail.com>
2018-11-29 00:02:31 -0500

    CSV handler - reader updated

---
+++ src/csvHandler/CSVreader.java
@@ -0,0 +1,21 @@
+package csvHandler;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class CSVreader {
+	final String FILE_NAME = "calendarData/userData.csv";
+
+	public void reader() {
+		File fileIn = new File(FILE_NAME);
+		try (Scanner in = new Scanner(fileIn)) {
+			while (in.hasNextLine()) {
+				System.out.println(in.nextLine());
+			}
+		} catch (FileNotFoundException ex1) {
+			System.out.printf("The path '%s' is invalid. Refer to 'csvHandler/CSVhandler'.", FILE_NAME);
+			System.exit(5);
+		}
+	}
+}
